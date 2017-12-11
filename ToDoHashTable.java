2164142ce990921c2502855a337e863128927e25
sandyljiang <sandyljiang@users.noreply.github.com>
2017-12-11 18:40:46 -0500

    Add files via upload

--- ToDoHashTable.java
+++ ToDoHashTable.java
@@ -11,11 +11,12 @@ import java.util.Scanner;
 
 public class ToDoHashTable<K,E>
 {
-    
+  public static Enumeration numbers; 
   public static void main (String[] args) throws IOException
   {
-    File file = new File("C:\\Users\\samor\\Documents\\NetBeansProjects\\SecondEECS233\\src\\FinalProject\\ClassList.txt");
-      System.out.println("This is our list of classes that need to be taken");
+    
+    File file = new File("/Users/Sandy/Desktop/EECS233.txt");
+    System.out.println("This is our list of classes that need to be taken");
     Scanner stdin = new Scanner(System.in);
     String expression;
     System.out.println("What major are you?");
@@ -24,43 +25,49 @@ public class ToDoHashTable<K,E>
     if (!rightMajor(expression))
     {
       System.out.println("That major is not supported by our code currently");
+      query(stdin, "Do you want to try again?");
     }
     else{
       System.out.println("Wow a computer science major! We only support BS majors for software engineering track! ");
       query(stdin, "Is that okay?");
-    Hashtable hash = creatingHashTable(file);
-    
-    do{
-     System.out.print("Name 1 computer science class you have taken: ");
-     expression = stdin.nextLine( ).toUpperCase( );
-     boolean removed = false;
-     
-     for(int i = 0; i<hash.size(); i++)
-     {
-        removed = removingClasses(expression,i, hash);
-     }
-     if(removed)
-       System.out.println("Class removed from recommended class list!");
-     else
-        System.out.println("That's not a computer science class!");
-    } 
-    while (query(stdin, "Do you have another class?"));
-    System.out.println("Thanks for letting us help! Here is your class list!");
-    computeClassRecommendations(hash);
+      Hashtable hash = creatingHashTable(file);
+      
+      do{
+        System.out.print("Name 1 computer science class you have taken: ");
+        expression = stdin.nextLine( ).toUpperCase( );
+        boolean removed = false;
+        
+        while(numbers.hasMoreElements()) 
+        {
+          int key = (Integer)numbers.nextElement();
+          if((String)hash.get(key) != (String) expression.toUpperCase())
+          {
+            removed = removingClasses(expression, key, hash); 
+          }
+        }
+        if(removed)
+          System.out.println("Class removed from recommended class list!");
+        else
+          System.out.println("That's not a computer science class!");
+      } 
+      while (query(stdin, "Do you have another class?"));
+      System.out.println("Thanks for letting us help! Here is your class list!");
+      computeClassRecommendations(hash);
     }
   }//end main
-
+  
+  
+  // it is not removing
   public static boolean removingClasses(String expression, int key, Hashtable hash){
-      long start,end;
-      start = System.currentTimeMillis();
-      if(hash.get(key) != null){
-          hash.remove(key);
-          return true;
-      }
-      end = System.currentTimeMillis();
-      System.out.println("It took " + (end-start) + " milliseconds to finish removing classes");
-      return false;
-
+    long start,end;
+    start = System.currentTimeMillis();    
+    if(hash.get(key) != null){
+      hash.remove(key);
+      return true;
+    }
+    end = System.currentTimeMillis();
+    System.out.println("It took " + (end-start) + " milliseconds to finish removing classes");
+    return false;
   }
   
   //queries user whether they want to continue
@@ -69,16 +76,16 @@ public class ToDoHashTable<K,E>
     String answer;
     System.out.print(prompt + " [Y or N]: ");
     answer = input.nextLine( ).toUpperCase( );
-
+    
     while (!answer.startsWith("Y") && !answer.startsWith("N"))
     {
       System.out.print("Invalid response. Please type Y or N: ");
       answer = input.nextLine( ).toUpperCase( );
     }
     return answer.startsWith("Y");
-
+    
   }//end query
-
+  
   //checks if right major for user
   public static boolean rightMajor(String expression )
   {
@@ -87,11 +94,11 @@ public class ToDoHashTable<K,E>
     else
       return false;
   }
- 
+  
   //creates hashtable and fills it with items from file
   private static Hashtable creatingHashTable(File fin) throws IOException {
     FileInputStream fis = new FileInputStream(fin);
-    Hashtable<Integer,String> hash = new Hashtable<>();
+    Hashtable<Integer,String> hash = new Hashtable<Integer,String>(100);
     BufferedReader br = new BufferedReader(new InputStreamReader(fis));
     int i = 0;
     String line;
@@ -101,16 +108,17 @@ public class ToDoHashTable<K,E>
       i++;
     }
     br.close();
+    numbers = hash.keys();
     return hash;
   }
   
   //computes recommended class
   public static void computeClassRecommendations(Hashtable hash){
-      long start,end;
-      start = System.currentTimeMillis();
-      System.out.println(hash.toString());
-      end = System.currentTimeMillis();
-      System.out.println("It took " + (end-start) + " milliseconds to finish computing classes");
+    long start,end;
+    start = System.currentTimeMillis();
+    System.out.println(hash.toString());
+    end = System.currentTimeMillis();
+    System.out.println("It took " + (end-start) + " milliseconds to finish computing classes");
   }
-
+  
 }
