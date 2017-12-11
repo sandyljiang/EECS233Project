36792fccf9fe74bc3a7fee49429304f3c5618b6b
sfrankum <samo.rocks@hotmail.com>
2017-12-11 01:10:24 -0500

    Update ToDoHashTable.java

--- ToDoHashTable.java
+++ ToDoHashTable.java
@@ -1,4 +1,3 @@
-
 //Sandy Jiang (sxj409) and Samantha Frankum (srf48)
 
 import java.io.BufferedReader;
@@ -7,24 +6,19 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
 import java.util.Scanner;
 
 
 public class ToDoHashTable<K,E>
-
 {
-  
+    
   public static void main (String[] args) throws IOException
   {
-    
-    File file = new File("/Users/Sandy/Desktop/EECS233.txt");
+    File file = new File("C:\\Users\\samor\\Documents\\NetBeansProjects\\SecondEECS233\\src\\FinalProject\\ClassList.txt");
       System.out.println("This is our list of classes that need to be taken");
-
     Scanner stdin = new Scanner(System.in);
     String expression;
     System.out.println("What major are you?");
-
     System.out.print("Your major: ");
     expression = stdin.nextLine( ).toUpperCase( );
     if (!rightMajor(expression))
@@ -34,18 +28,16 @@ public class ToDoHashTable<K,E>
     else{
       System.out.println("Wow a computer science major! We only support BS majors for software engineering track! ");
       query(stdin, "Is that okay?");
-    
-    
     Hashtable hash = creatingHashTable(file);
+    
     do{
      System.out.print("Name 1 computer science class you have taken: ");
      expression = stdin.nextLine( ).toUpperCase( );
-     
      boolean removed = false;
      
      for(int i = 0; i<hash.size(); i++)
      {
-     removingClasses(expression,i, hash);
+        removed = removingClasses(expression,i, hash);
      }
      if(removed)
        System.out.println("Class removed from recommended class list!");
@@ -53,25 +45,28 @@ public class ToDoHashTable<K,E>
         System.out.println("That's not a computer science class!");
     } 
     while (query(stdin, "Do you have another class?"));
-    
     System.out.println("Thanks for letting us help! Here is your class list!");
     computeClassRecommendations(hash);
     }
   }//end main
 
   public static boolean removingClasses(String expression, int key, Hashtable hash){
+      long start,end;
+      start = System.currentTimeMillis();
       if(hash.get(key) != null){
           hash.remove(key);
           return true;
       }
+      end = System.currentTimeMillis();
+      System.out.println("It took " + (end-start) + " milliseconds to finish removing classes");
       return false;
+
   }
   
-  
+  //queries user whether they want to continue
   public static boolean query(Scanner input, String prompt)
   {
     String answer;
-
     System.out.print(prompt + " [Y or N]: ");
     answer = input.nextLine( ).toUpperCase( );
 
@@ -80,41 +75,42 @@ public class ToDoHashTable<K,E>
       System.out.print("Invalid response. Please type Y or N: ");
       answer = input.nextLine( ).toUpperCase( );
     }
-    
     return answer.startsWith("Y");
+
   }//end query
 
-  //write all your code here
-
+  //checks if right major for user
   public static boolean rightMajor(String expression )
   {
     if(expression.equalsIgnoreCase("computer science"))
       return true;
     else
       return false;
-
   }
-
-  
+ 
+  //creates hashtable and fills it with items from file
   private static Hashtable creatingHashTable(File fin) throws IOException {
     FileInputStream fis = new FileInputStream(fin);
-    Hashtable<Integer,String> hash = new Hashtable<Integer,String>();
-    
+    Hashtable<Integer,String> hash = new Hashtable<>();
     BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-
     int i = 0;
-    String line = null;
+    String line;
+    
     while ((line = br.readLine()) != null) {
       hash.put(i, line);
       i++;
     }
-   
     br.close();
     return hash;
   }
   
+  //computes recommended class
   public static void computeClassRecommendations(Hashtable hash){
+      long start,end;
+      start = System.currentTimeMillis();
       System.out.println(hash.toString());
+      end = System.currentTimeMillis();
+      System.out.println("It took " + (end-start) + " milliseconds to finish computing classes");
   }
 
 }
